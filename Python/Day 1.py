2b768f8474f4b22f91ec416bc742787dfe829f30
imam4diamondpeace-lang <imam4diamondpeace@gmail.com>
2025-11-17 14:13:25 +0100

    Add files via upload
    Add Day 1 and Day 2 python files

---
+++ Python/Day 1.py
@@ -0,0 +1,22 @@
+# print("Hello Muhammad")
+# print("I am starting AI training today")
+# print(5 + 5)
+# print(5 - 5)
+# name = "Muhammad"
+# event ="I am starting python today May Allah make it easy"
+# print(name, "Muneer",event, " Aameen")
+# your_name = input("Enter your name: ")
+# your_goal = input("Enter your goal: ")
+# print("welcome,",your_name, "How can I help you being",your_goal )
+# #Task
+# name =input("Enter your name: ")
+# age = input("Enter your age: ")
+# fav_num = input("Enter your favorite number: ")
+# print("Hello,",name,"You are",age, "and your favorite number is",fav_num)
+a = 12
+b = 4
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("I love Mathematics \nI want to build my own AT \nI want to help my community ")
---
+++ Python/Day 2.py
@@ -0,0 +1,25 @@
+# a = 20
+# b = 5
+# c = a + b
+# print("Sum of a and b:", c)
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("You are an adult")
+# else:
+#     print("You are a minor")
+
+# name = input("Enter your name: ")
+# print("Hello",name,"Welcome to python" )
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print(num1 + num2 )
+print(num1 - num2 )
+print(num1 * num2 )
+print(num1 / num2 )
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are adult")
+else:
+    print("You are minor")
+food = input("Enter your favorite food: ")
+print(food, "is very delicious")
