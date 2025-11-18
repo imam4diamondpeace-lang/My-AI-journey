eecbc8a728e8af6878c072858ff9576d0ce6c9df
imam4diamondpeace-lang <imam4diamondpeace@gmail.com>
2025-11-18 15:25:06 +0100

    Create Day 3.py

---
+++ Python/Day 3.py
@@ -0,0 +1,42 @@
+# for i in range(1, 6):
+#     print("Counting:" ,i)
+# count =1
+# while count >=5:
+#     print("Counting:" ,count)
+#     count +=1
+# fruit =["Banana" , "Plantain" , "Mango"]
+# for i in fruit:
+#     print("I love " ,i)
+score = 0
+m = input("4 + 4 = ")
+if m == "8":
+    score += 1
+    print("Thats correct👍")
+else:
+    score
+    print("Not correct😞")
+print("score:" ,score,"/5")
+m = input("5 + 5 = ")
+if m == "10":
+    score += 1
+    print("Thats correct👍")
+else:
+    score
+    print("Not correct😞")
+print("score:" ,score,"/5")
+m = input("3 + 4 = ")
+if m == "7":
+    score += 1
+    print("Thats correct👍")
+else:
+    score
+    print("Not correct😞")
+print("score:" ,score,"/5")
+m = input("2 + 2 = ")
+if m == "4":
+    score += 1
+    print("Thats correct👍")
+else:
+    score
+    print("Not correct😞")
+print("score:" ,score,"/5")
